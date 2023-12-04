b29f8a10d430e05168b118ee219ab9833b8f31b9
i <i@ohyooo.com>
2023-12-04 13:28:34 +0800

    update ui

--- app/src/main/kotlin/com/ohyooo/quick/SplashActivity.kt
+++ app/src/main/kotlin/com/ohyooo/quick/SplashActivity.kt
@@ -3,6 +3,7 @@ package com.ohyooo.quick
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import com.ohyooo.kni.Kni
 import com.ohyooo.rust.librust
@@ -14,8 +15,10 @@ class SplashActivity : ComponentActivity() {
         Kni.sayHello()
 
         setContent {
-            Text(text = Kni.callFromNative())
-            Text(text = librust.hello("rust"))
+            Column {
+                Text(text = Kni.callFromNative())
+                Text(text = librust.hello("rust"))
+            }
         }
     }
 
